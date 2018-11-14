825d039b537620d61f584ab5a72821bea113be44
Hen Chow <henchow@gmail.com>
2018-11-14 15:31:00 +1100

    update linting rules

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: 'airbnb-base',
+
+  env: {
+    node: true,
+    mocha: true,
+  },
+
+
+  rules: {
+    'new-cap': 'off',
+
+    'arrow-body-style': 'off',
+
+    'no-new': 'off',
+
+    'object-curly-newline': 'off',
+  },
+};
